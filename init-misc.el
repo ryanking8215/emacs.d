e834b4a4957288e1c81db7b1394f0ce22b1b7548
ryanking <jzh_howell@126.com>
2013-07-01 21:55:07 +0800

    mine add cc and cedet

--- init-misc.el
+++ init-misc.el
@@ -20,4 +20,22 @@
 (setq default-fill-column 80);默认显示 80列就换行
 (global-linum-mode t)
 
+;;; my global keymap
+(global-set-key [f2] 'eshell);
+;; (global-set-key [f5] 'gdb);F5调试程序
+;; (setq compile-command "make -f Makefile")
+(global-set-key [f5] 'compile);F5编译文件
+(global-set-key [f7] 'shell-command);shell commond in minibuf
+(global-set-key [f8] 'other-window);F8窗口间跳转
+(global-set-key [C-return] 'ido-kill-buffer);C-return关闭当前buffer
+;; (global-set-key [f9] 'sr-speedbar-toggle)
+;; (global-set-key [f10] 'split-window-vertically);F10分割窗口
+;; (global-set-key [f11] 'delete-other-windows);F11 关闭其它窗口
+;; (global-set-key [f12] 'my-fullscreen);F12 全屏
+(global-set-key (kbd "C-,") 'backward-page);文件首
+(global-set-key (kbd "C-.") 'forward-page);文件尾
+(global-set-key (kbd "M-[") 'find-tag)
+(global-set-key (kbd "M-]") 'pop-tag-mark)
+(global-set-key (kbd "C-c m") 'magit-status)
+
 (provide 'init-misc)
