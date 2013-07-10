50b19043b241427fee864e38446c1ebda240c579
ryanking <jzh_howell@126.com>
2013-07-10 17:58:13 +0800

    add yasnippet

---
+++ init-yasnippet.el
@@ -0,0 +1,7 @@
+(require-package 'yasnippet)
+(require 'yasnippet) ;; not yasnippet-bundle
+(yas-global-mode t)
+(yas/load-directory "~/.emacs.d/elpa/yasnippet-20130505.2115/snippets")
+
+(provide 'init-yasnippet)
+
